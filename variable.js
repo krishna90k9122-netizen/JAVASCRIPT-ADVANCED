0a8fd34e2643a86604af5d13d726d47854955536
krishna90k9122-netizen <krishna90k9122@gmail.com>
2026-07-08 10:19:38 +0530

    variable

--- variable.js
+++ variable.js
@@ -2,3 +2,24 @@ let name;
 
 name="Krishna Gupta";
 console.log(name);
+
+let fullname="Krishnagupta";
+console.log(fullname);
+
+let age=30;
+console.log(age);
+
+let isAge=29;
+console.log(isAge);
+
+{
+    let areage=30;
+    console.log(areage);
+}
+// console.log(areage); // This will cause an error because 'areage' is not defined in this scope
+
+{
+    var aage=30;    
+    console.log(aage);
+}
+console.log(aage);
