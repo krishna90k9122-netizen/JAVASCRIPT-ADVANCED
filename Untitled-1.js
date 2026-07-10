629eacdcdc98500252863b3d0a083feac1763627
krishna90k9122-netizen <krishna90k9122@gmail.com>
2026-07-10 23:48:51 +0530

    opeartor

---
+++ Untitled-1.js
@@ -0,0 +1,6 @@
+console.log("hello world");
+
+let name;
+
+name="Krishna Gupta";
+console.log(name);
---
+++ operator.js
@@ -0,0 +1,7 @@
+// a+b=result
+// console.log(0/0)
+
+let rsult=10+20;
+console.log(rsult);
+
+// here the + is operator which is used to add two numbers. In this case, it adds 10 and 20, resulting in 30, which is then logged to the console.
