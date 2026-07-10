82fc8f35d51d8c54d1b638cb52ae93ba70a42b67
krishna90k9122-netizen <krishna90k9122@gmail.com>
2026-07-10 23:56:43 +0530

    console

--- operator.js
+++ operator.js
@@ -5,3 +5,8 @@ let rsult=10+20;
 console.log(rsult);
 
 // here the + is operator which is used to add two numbers. In this case, it adds 10 and 20, resulting in 30, which is then logged to the console.
+
+console.log("6+1");
+console.log(6+1);
+
+
