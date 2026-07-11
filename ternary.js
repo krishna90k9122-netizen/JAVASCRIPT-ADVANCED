37d47cff63b172520e09c890ce8af0af4a8a30fa
krishna90k9122-netizen <krishna90k9122@gmail.com>
2026-07-11 21:08:05 +0530

    bitwise

---
+++ ternary.js
@@ -0,0 +1,14 @@
+// let age=50;
+
+// let result=age>18? "Eligible to vote":"Not Eligible to vote";
+// console.log(result);
+
+// let username="r";
+// let password="h";
+
+// const result=(username && password) ?("login successful") :("login failed");
+// console.log(result);
+
+let a=10 , b=2 , c=3;
+let result=a>b? (a>c? a :c ) : (b>c ? b : c);
+console.log(result);
