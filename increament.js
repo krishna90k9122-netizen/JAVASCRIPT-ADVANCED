06b055abcdf0764f986919f2dab483407a306141
krishna90k9122-netizen <krishna90k9122@gmail.com>
2026-07-11 20:16:35 +0530

    oparator

---
+++ increament.js
@@ -0,0 +1,16 @@
+// a=10;
+// console.log(a++)    
+
+// console.log(a)
+//  console.log(++a)
+//  console.log(a)
+
+//  console.log(a--)
+//  console.log(a)
+//  console.log(--a)
+//  console.log(a)
+
+ let a=5;
+
+ let result = a++ + ++a + a-- + --a;
+console.log(result)
---
+++ logical.js
@@ -0,0 +1,13 @@
+console.log(true && true);
+
+console.log(true && false);
+
+console.log(!true);
+
+let x=20;
+
+console.log(x>10 && x<30);
+
+console.log(x>50 || x<10);
+
+console.log(!(x>10 && x<30));
