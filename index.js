b94b783f7f53af7baa4bd89ddd14f518d7dc53bc
krishna90k9122-netizen <krishna90k9122@gmail.com>
2026-07-11 21:24:37 +0530

    question

---
+++ index.js
@@ -0,0 +1,13 @@
+// let num1=Number(prompt("Enter first number"));
+// let num2=Number(prompt("Enter second number"));
+
+
+// console.log("The sum of two numbers is: " + (num1+num2));
+// console.log("The difference of two numbers is: " + (num1-num2));
+// console.log("The product of two numbers is: " + (num1*num2));
+// console.log("The quotient of two numbers is: " + (num1/num2));
+
+
+let num=Number(prompt("Enter first number"));
+let result=(num % 2 === 0) ? "Even" : "Odd";
+console.log(result);
